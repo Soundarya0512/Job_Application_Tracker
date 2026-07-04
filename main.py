c2bec82fbe94c944f94f5ce1026e96f63f0607b8
Soundarya <soundaryakishore05@gmail.com>
2026-07-03 20:43:15 -0700

    Add events table, FastAPI-Supabase connection, and event read/write endpoints

--- main.py
+++ main.py
@@ -1,7 +1,31 @@
 from fastapi import FastAPI
-
+from database import supabase
+from pydantic import BaseModel
 app = FastAPI()
+
+class CreateEvent(BaseModel):
+    application_id :str
+    event_type : str
+    payload: dict 
+    source:str
 
 @app.get("/health")
 def health():
     return {"status": "ok"}
+
+
+
+@app.get("/db_events")
+def get_events():
+    response = supabase.table("events").select("*").execute()
+    return response.data
+
+@app.post("/events")
+def db_insert(event: CreateEvent):
+    response = supabase.table("events").insert({
+        "application_id": event.application_id,
+        "event_type": event.event_type,
+        "payload": event.payload,
+        "source": event.source
+    }).execute()
+    return response.data    
